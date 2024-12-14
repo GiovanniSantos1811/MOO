98f06d2f4d15b5017cbb0a978340bdaebd8d33cd
Giovanni <giovanni.santos.1811@usp.br>
2024-12-14 02:08:27 -0300

    Adicionada funcionalidade de criação de convites

--- package/app/views.py
+++ package/app/views.py
@@ -1,7 +1,23 @@
 from django.shortcuts import render, redirect
 from django.http import HttpResponse
-from .models import Perfil_Jogador
+from .models import Perfil_Jogador, Jogo, Convite
 from django.contrib import messages
+from datetime import date
+
+#Auxiliar functions
+def age(data):
+    # Obter a data atual
+    data_atual = date.today()
+    
+    # Calcular a diferença em anos
+    idade = data_atual.year - data.year
+    
+    # Ajustar caso o aniversário ainda não tenha ocorrido no ano atual
+    if (data_atual.month, data_atual.day) < (data.month, data.day):
+        idade -= 1  # Não fez aniversário ainda este ano
+    
+    return idade
+
 
 # Create your views here.
 def home(request):
@@ -60,4 +76,42 @@ def convites(request, apelido):
     return render(request, "convites.html", {"apelido": apelido})
 
 def criacao(request, apelido):
-    return render(request, "criacao.html", {"apelido": apelido})
+    #Passando os jogos armazenados no banco de dados para o select da página
+    jogos = Jogo.objects.all() #Selecionando todos os jogos disponíveis
+
+    if request.method == "POST":
+        jogador2 = request.POST.get('apelido')
+        jogo_id = request.POST.get('titulo_jogo')
+        data_convite = request.POST.get('data_convite')
+        mensagem = request.POST.get('mensagem')
+
+        #Checagem da existência do jogador2
+        try:
+            perfil = Perfil_Jogador.objects.get(apelido=jogador2)
+            jogo = Jogo.objects.get(id=jogo_id)
+            
+            #Garantindo que o usuário não esteja tentando mandar convite para si mesmo
+            if (jogador2 != apelido):
+                #Garantindo que ambos os jogadores tenham idade suficiente para a classificação indicativa do jogo selecionado
+                idade1 = age(Perfil_Jogador.objects.get(apelido=apelido).data_nascimento)
+                idade2 = age(perfil.data_nascimento)
+                if (idade1 >= jogo.classificacao_etaria and idade2 >= jogo.classificacao_etaria):
+                    #Criando um convite novo na tabela de Convites
+                    convite = Convite.objects.create(
+                        jogador1 = Perfil_Jogador.objects.get(apelido=apelido), #Jogador que está enviando o convite
+                        jogador2 = perfil, #Jogador que está recebendo o convite
+                        jogo = jogo,
+                        data_convite = data_convite,
+                        mensagem = mensagem
+                    )
+
+                    return redirect('convites', apelido=apelido)
+
+                else:
+                    messages.error(request, "O jogo escolhido é inapropriado para a idade de um ou mais jogadores")
+            else:
+                messages.error(request, "O convite precisa ser feito para um outro jogador!")
+        except Perfil_Jogador.DoesNotExist:
+            messages.error(request, 'Jogador não encontrado')
+    
+    return render(request, "criacao.html", {"apelido": apelido, "jogos":jogos}) #Renderizando a página com as informações dos jogos cadastrados
